7845f141b7277e1dd43f511b56d05988ba8611b8
ww2or3ww <w2or3w@gmail.com>
2020-12-29 13:35:50 +0000

    sort distance

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 
 ## ライブラリのインストール
-$ pip install -r requirements.txt -t source_xxx
+$ pip install -r requirements.txt -t source
 
 
 ## パッケージング&デプロイ コマンド
 
 ```
 $ find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-$ cd source_xxx
+$ cd source
 $ zip -r ../lambda-package.zip *
-$ aws lambda update-function-code --function-name near-near-map-search --zip-file fileb://../lambda-package.zip
+$ aws lambda update-function-code --function-name near-near-map-search-dev --zip-file fileb://../lambda-package.zip
 ```
--- source/lambda_function.py
+++ source/lambda_function.py
@@ -14,6 +14,7 @@ from retry import retry
 import boto3
 from boto3.dynamodb.conditions import Key
 import googlemaps
+from geopy.distance import geodesic
 
 import logging
 logger = logging.getLogger()
@@ -32,11 +33,29 @@ def lambda_handler(event, context):
     try:
         logger.info("=== START ===")
         types = event["queryStringParameters"]["type"]
-        latlon = event["queryStringParameters"]["latlon"]
-        zoom = int(event["queryStringParameters"]["zoom"])
-        logger.info("type={0}, latlon={1}, zoom={2}".format(types, latlon, zoom))
+        latlon = None
+        if "latlon" in event["queryStringParameters"]:
+            latlon = event["queryStringParameters"]["latlon"]
+        address = None
+        if "address" in event["queryStringParameters"]:
+            address = event["queryStringParameters"]["address"]
+        zoom = 16
+        if "zoom" in event["queryStringParameters"]:
+            zoom = int(event["queryStringParameters"]["zoom"])
+        count = RESULT_COUNT_MAX
+        if "count" in event["queryStringParameters"]:
+            count = int(event["queryStringParameters"]["count"])
+        isSort = False
+        if "sort" in event["queryStringParameters"]:
+            isSort = str2bool(event["queryStringParameters"]["sort"])
+        
+        if not latlon and address:
+            latlon = getLatLonFromAddress(address)
 
-        hits = search_h3(types, latlon, zoom)
+        message = "type={0}, latlon={1}, address={2}, zoom={3}, count={4}, sort={5}".format(types, latlon, address, zoom, count, isSort)
+        logger.info(message)
+        
+        hits = search_h3(types, latlon, zoom, count, isSort)
 
         result = {}
         has_clowd = False
@@ -84,7 +103,25 @@ def lambda_handler(event, context):
             "body": "error"
         }
 
-def search_h3(types, latlon, zoom):
+@retry(tries=3, delay=1)
+def getLatLonFromAddress(address):
+    try:
+        latlon = None
+        result = GMAPS.geocode(address)
+        try:
+            logger.info(result)
+            lat = result[0]["geometry"]["location"]["lat"]
+            lng = result[0]["geometry"]["location"]["lng"]
+            latlon = "{0},{1}".format(lat, lng)
+        except Exception as e:
+            logger.warn("can't get geocode : {0}".format(address))
+            
+        return latlon
+    except Exception as e:
+        logger.error(address)
+        logger.exception(e)
+
+def search_h3(types, latlon, zoom, count, isSort):
     if zoom <= 13:
         h3type = 7
     elif zoom >= 14 and zoom <= 15:
@@ -98,15 +135,34 @@ def search_h3(types, latlon, zoom):
     logger.info(rings)
 
     results = []
+    ringIndex = 0
     for sdata in rings:
         for h3index in sdata:
             response = query_dynamodb(types, h3index, h3type)
             results.extend(response)
-        if len(results) >= RESULT_COUNT_MAX:
+        logger.info("ringIndex={0}, resultCount={1}".format(ringIndex, len(results)))
+        if len(results) >= count and ringIndex > 0:
             break
+        ringIndex = ringIndex + 1
+        
     logger.info("result size = {0}".format(len(results)))
 
+    results = map(lambda data: calcDistanceFrom(latlon, data), results)
+    results = list(results)
+    
+    if isSort:
+        results = sorted(results, key=lambda data: data["distance"])
+        if count > 0 and len(results) > count:
+            results = results[0 : count]
+    
     return results
+
+def calcDistanceFrom(latlon, data):
+    latlon_ary1 = latlon.split(",")
+    latlon_ary2 = data["latlon"].split(",")
+    distance = geodesic(latlon_ary1, latlon_ary2).km
+    data["distance"] = distance
+    return data
 
 @retry(tries=3, delay=1)
 def query_dynamodb(types, h3index, h3type):
@@ -230,6 +286,7 @@ def convert(types, item):
     child["title"] = item["title"]
     child["tel"] = item["tel"]
     child["address"] = item["address"]
+    child["distance"] = item["distance"]
 
     if "image" in item and item["image"]:
         child["image"] = urljoin("https://near-near-map.s3-ap-northeast-1.amazonaws.com/", item["image"])
@@ -277,3 +334,6 @@ def convert(types, item):
     data["list"].append(child)
     
     return data
+    
+def str2bool(s):
+     return s.lower() in ["true", "t", "yes", "1"]
